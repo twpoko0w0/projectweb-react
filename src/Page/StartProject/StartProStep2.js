fe61c098309d09d90677289d2d21b2b693d1784a
twpoko <tawan.hira@bumail.net>
2022-04-07 22:54:09 +0700

    [ADD] StartProStep for web_react

---
+++ src/Page/StartProject/StartProStep2.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function StartProStep2({ formdata, setFormdata }) {
+  const { projectSeriousnessId } = formdata;
+  const projectSeriousnessToInt = parseInt(projectSeriousnessId);
+  // console.log("This is project SeId: " + projectSeriousnessId);
+
+  return (
+    <>
+      <div className="container-grid">
+        <h1 className="d-flex justify-content-center">ชั้นตอนที่ 2: เลือกระดับของโปรเจค</h1>
+        <p className="d-flex justify-content-center text-secondary"><h3>เลือกระดับเพื่อค้นหาทีมที่เหมาะกับโปรเจคของเรา</h3></p>
+        <Form.Group className="mt-5">
+          <Form.Check type="radio" id={1} name="projectSeriousnessId" value={1} inline onChange={(e) => setFormdata({ ...formdata, projectSeriousnessId: e.target.value })} checked={projectSeriousnessToInt === 1} />
+          <label for={1}><b>งานอดิเรก:</b> เป็นประเภทที่ไม่จริงจังมากเกินไป อยากทดสอบไอเดียของโปรเจคว่าเป็นจริงได้มากแค่ไหน</label><br />
+          <Form.Check type="radio" id={2} name="projectSeriousnessId" value={2} inline onChange={(e) => setFormdata({ ...formdata, projectSeriousnessId: e.target.value })} className="mt-4" checked={projectSeriousnessToInt === 2} />
+          <label for={2}><b>ผลงาน:</b> เป็นประเภทที่จริงจังกับงาน อยากให้งานชิ้นนี้เกิดขึ้นจริง เพื่อเก็บเป็นผลงานของตัวเอง</label><br />
+        </Form.Group>
+      </div>
+    </>
+  );
+}
+
+export default StartProStep2;
---
+++ src/Page/StartProject/StartProStep4.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+import { axios } from "axios";
+import { Button, Col, Dropdown, Form, ListGroup, Row, Spinner } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Navbar1 from "../../Component/Navbar";
+import Add from "../../Component/logo/add_circle_black_24dp.svg";
+import More from "../../Component/logo/more_horiz_black_24dp.svg";
+import styled from "styled-components";
+
+
+const Stylelist = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+  .list-group {
+    width: 736px;
+    background: #fff;
+    box-shadow: 0px 16px 48px rgba(0, 0, 0, 0.176);
+  }
+
+  .list-group-item.hearder {
+    padding: 30px;
+    height: 95px;
+    border-left: none;
+    border-right: none;
+    border-top: none;
+  }
+  h6 {
+    margin: 0;
+    padding-top: 4px;
+    font-family: "Roboto" sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+  }
+  .list-group-item.item {
+    display: flex;
+    justify-content: space-between;
+    height: 48px;
+    border-left: none;
+    border-right: none;
+  }
+  span {
+    padding: 0;
+    align-self: center;
+  }
+  .name {
+    margin: 0;
+    align-self: center;
+    color: #3082fe;
+  }
+  .material-icons {
+    float: right;
+    color: #000;
+    margin-left: 20px;
+  }
+`;
+
+function StartProStep4({ formdata, setFormdata, projectTag, tagList, newProjectId, forceUpdate }) {
+  const { projectTagId, projectRole, projectPositionQuantity, } = formdata;
+
+  // console.log("Quantity position: " + projectPositionQuantity + "\nTagID: " + projectTagId);
+
+  const projectTagElement = projectTag.map((val, index) => {
+    return (
+      <option key={index} value={val.id}>{val.project_tag_name}</option>
+    )
+  });
+
+  const tagElement = tagList.map((val) => {
+    return (
+      <ListGroup.Item className="item">
+        <span>{val.projectTagName}</span>
+        <p className="name">
+          {val.projectPositionQuantity + " ตำแหน่ง"} <span class="material-icons"><img src={More} alt="More" /></span>
+        </p>
+      </ListGroup.Item>
+    )
+  })
+  // console.log("Here: " + formdata.projectTagId)
+  const AddList = (e) => {
+    e.preventDefault();
+    forceUpdate();
+    let tagToInt = parseInt(projectTagId)
+    let projectTagName = projectTag.find(x => x.id === tagToInt).project_tag_name
+    let list = {
+      projectTagId: projectTagId,
+      projectRole: projectRole,
+      projectPositionQuantity: projectPositionQuantity,
+      projectTagName: projectTagName
+    }
+    tagList.push(list);
+    // console.log("Tag Name: " + projectTagName);
+  }
+  console.log(projectTagId)
+  return (
+    <div >
+      <h1 className="d-flex justify-content-center">ชั้นตอนที่ 4: ตามหาตำแหน่งที่ต้องการ</h1>
+      <h5 className="d-flex justify-content-center">กำหนดตำแหน่งที่คุณต้องการ เพื่อร่วมสร้างโปรเจคกับคุณ</h5>
+      <Form as={Row} className="mt-5">
+        <div className="shadow p-3 mb-5 bg-body rounded">
+          <Row>
+            <Col xs={12} md={10}>
+              {" "}
+              <h5><b>กรอกจำนวนตำแหน่ง</b></h5>
+            </Col>
+            <Col xs={12} md={2}>
+              <Form.Select name="projectPositionQuantity" value={projectPositionQuantity} onChange={(e) => setFormdata({ ...formdata, projectPositionQuantity: e.target.value })}>
+                <option value={1}>1 ตำแหน่ง</option>
+                <option value={2}>2 ตำแหน่ง</option>
+                <option value={3}>3 ตำแหน่ง</option>
+                <option value={4}>4 ตำแหน่ง</option>
+                <option value={5}>5 ตำแหน่ง</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Form.Label>ตำแหน่ง</Form.Label>
+          <Form.Select value={projectTagId} name="projectTagId" onChange={(e) => setFormdata({ ...formdata, projectTagId: e.target.value })}>
+            {projectTagElement}
+          </Form.Select>
+          <Form.Label className="mt-4">ตำแหน่งนี้มีหน้าที่อะไร</Form.Label>
+          <Form.Control label="projectRole" name="projectRole" type="text" value={projectRole} placeholder="" onChange={(e) => setFormdata({ ...formdata, projectRole: e.target.value })} as="textarea"></Form.Control>
+          <Button
+            className="Plus mt-4"
+            type="addicon"
+            variant="primary"
+            style={{ width: "124px", height: "44px", padding: "10px" }}
+            onClick={AddList}
+          >
+            <img src={Add} alt="ADD" />
+            เพิ่มลงลิสต์
+          </Button>
+        </div>
+
+      </Form>
+      <div className="list-box" style={{ marginTop: "40px" }}>
+        <Stylelist>
+          <div>
+            <h5><b>หน้าที่ในลิสท์</b></h5>
+            <ListGroup className="mt-3">
+              {tagElement}
+            </ListGroup>
+          </div>
+        </Stylelist>
+      </div>
+    </div>
+  );
+}
+
+export default StartProStep4;
